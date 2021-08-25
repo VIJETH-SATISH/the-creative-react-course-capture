0077763a59973586463864da0a82d4ea40eace8c
Vijeth56 <vijeth2011@gmail.com>
2021-08-25 20:43:29 +0530

    2.About Section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import AboutSection from "./components/AboutSection";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
-
 
 export default App;
 
