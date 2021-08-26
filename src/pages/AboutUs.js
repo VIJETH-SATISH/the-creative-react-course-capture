45bd87fea9ee61ccc74526d0039bc9db554e955b
Vijeth56 <vijeth2011@gmail.com>
2021-08-26 10:18:34 +0530

    4. FaqSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSections from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
@@ -8,6 +9,7 @@ const AboutUs = () => {
       {/* incase we wish not to use the div tag to wrap the components on page then we can use fractions <> </> */}
       <AboutSection />
       <ServicesSections />
+      <FaqSection />
     </>
   );
 };
